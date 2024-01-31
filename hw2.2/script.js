6f91a4cc6001561b59f819f53e58abd2c4b22e53
Anastasia <petrovaanast2020@gmail.come>
2024-01-31 18:38:17 +0300

    Variables and data types

---
+++ hw2.2/script.js
@@ -0,0 +1,72 @@
+// Переменные и типы данных
+
+// Task 1
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+
+// Task 2
+let yearReleaseIPhone = 'Год выпуска первого IPhone - 2007 год';
+alert(yearReleaseIPhone);
+
+// Task 3
+let creatorJS = 'Создатель JavaScript - Брендан Эйх';
+alert(creatorJS);
+
+// Task 4
+let one = 10;
+let two = 2;
+alert(10 + 2);
+alert(10 - 2);
+alert(10 * 2);
+alert(10 / 2);
+
+// Task 5
+let the = 2;
+result = 2 ** 5;
+alert(`2 в 5 степени = ${result}`);
+
+// Task 6
+let nine = 9;
+let number = 2;
+result = 9 % 2;
+alert(`Остаток = ${result}`);
+
+// Task 7
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+++num;
+--num;
+alert(num);
+
+// Task 8
+const age = prompt('Сколько вам лет?');
+alert(age);
+
+// Task 9.0
+let user = {
+    firstname: "",
+    age: Number,
+    isAdmin: true,
+};
+
+// Task 9.1
+user.cityOfResidence = true;
+
+// Task 9.2
+user.age = false;
+
+// Task 9.3
+delete user.cityOfResidence;
+
+// Task 9.4
+let info = prompt("Какую информацию хотите узнать о пользователе?");
+alert(user.info);
+
+// Task 10
+let youName = prompt('Ваше имя');
+alert(`Привет, ${youName}!`);
