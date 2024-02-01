d6c5aae9d77db8b7d0e1be1e37e3ffd76e51a9d0
Anastasia <petrovaanast2020@gmail.come>
2024-02-01 19:31:35 +0300

    Измененное дз

--- hw2.2/script.js
+++ hw2.2/script.js
@@ -15,22 +15,22 @@ let creatorJS = 'Создатель JavaScript - Брендан Эйх';
 alert(creatorJS);
 
 // Task 4
-let one = 10;
+let ten = 10;
 let two = 2;
-alert(10 + 2);
-alert(10 - 2);
-alert(10 * 2);
-alert(10 / 2);
+alert(ten + two);
+alert(ten - two);
+alert(ten * two);
+alert(ten / two);
 
 // Task 5
-let the = 2;
-result = 2 ** 5;
+let e = 2;
+result = e ** 5;
 alert(`2 в 5 степени = ${result}`);
 
 // Task 6
 let nine = 9;
 let number = 2;
-result = 9 % 2;
+result = nine % number;
 alert(`Остаток = ${result}`);
 
 // Task 7
@@ -49,23 +49,23 @@ alert(age);
 
 // Task 9.0
 let user = {
-    firstname: "",
-    age: Number,
+    name: 'Anastasia',
+    age: 24,
     isAdmin: true,
 };
 
 // Task 9.1
-user.cityOfResidence = true;
+user["city of residence"] = 'Sankt Petersburg';
 
 // Task 9.2
-user.age = false;
+user.age = 27;
 
 // Task 9.3
-delete user.cityOfResidence;
+delete user["city of residence"];
 
 // Task 9.4
-let info = prompt("Какую информацию хотите узнать о пользователе?");
-alert(user.info);
+let info = prompt("Какую информацию хотите узнать о пользователе?", "name", "age", "iAdmine");
+alert(user[info]);
 
 // Task 10
 let youName = prompt('Ваше имя');
